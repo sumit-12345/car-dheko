26fa65107aaed6579f4d15f32752dc503007291b
SumitSibaniSambit <sambitj69@gmail.com>
2021-10-05 20:51:13 +0530

    Attributes added to AbstractUsedCar

--- src/main/java/com/vehicle/car/AbstractUsedCar.java
+++ src/main/java/com/vehicle/car/AbstractUsedCar.java
@@ -1,5 +1,12 @@
 package com.vehicle.car;
 
+import java.util.Date;
+
 public abstract class AbstractUsedCar implements UsedCar {
 
+    double sellingPrice;
+    double kmDriven;
+    String owner;
+    String ownerAddress;
+    String year;
 }
