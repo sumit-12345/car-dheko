d0e7a34d0eb2614f612ef0be8d09d5c2ebc72d4c
sumit-12345 <sumit.joshi@gmail.com>
2021-10-07 08:17:34 +0530

    added specification as super interface

--- src/main/java/com/vehicle/car/UsedCar.java
+++ src/main/java/com/vehicle/car/UsedCar.java
@@ -3,7 +3,7 @@ package com.vehicle.car;
 import com.vehicle.model.CustomerDetails;
 import com.vehicle.model.Invoice;
 
-public interface UsedCar {
+public interface UsedCar extends Specification{
     Invoice sellCar(CustomerDetails customerDetails);
 
     boolean homeDelevery(CustomerDetails customerDetails);
